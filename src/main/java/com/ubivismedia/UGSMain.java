5cc1dfafc72edb23050169b2b2c3d6a83bbfbb6a
Ubivis <gunnar.steincke@hamburg.de>
2025-02-26 09:35:31 +0100

    Update UGSMain.java

--- src/main/java/com/ubivismedia/UGSMain.java
+++ src/main/java/com/ubivismedia/UGSMain.java
@@ -28,25 +28,3 @@ public class UGSMain extends JavaPlugin {
     }
 }
 
-// ReloadCommand.java
-package com.ubivismedia.command;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import com.ubivismedia.extension.ExtensionManager;
-
-public class ReloadCommand implements CommandExecutor {
-    private final ExtensionManager extensionManager;
-
-    public ReloadCommand(ExtensionManager extensionManager) {
-        this.extensionManager = extensionManager;
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        extensionManager.reloadExtensions();
-        sender.sendMessage("§aAlle Extensions wurden neu geladen.");
-        return true;
-    }
-}
