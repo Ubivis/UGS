27e1ac80a0c37f2762ccb759c20642e10e830145
Ubivis <gunnar.steincke@hamburg.de>
2025-02-26 09:03:59 +0100

    Create UGSMain.java

---
+++ src/main/java/com/ubivismedia/UGSMain.java
@@ -0,0 +1,26 @@
+package com.ubivismedia;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UGSMain extends JavaPlugin {
+    private ExtensionManager extensionManager;
+    
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        this.extensionManager = new ExtensionManager(this);
+        
+        getCommand("ugs").setExecutor(new UGSCommand(extensionManager));
+        getCommand("ugs reload").setExecutor(new ReloadCommand(extensionManager));
+        
+        getLogger().info("UGS Plugin aktiviert!");
+    }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("UGS Plugin deaktiviert!");
+    }
+}
